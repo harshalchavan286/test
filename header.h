051b8a0745dffb09a23cb4a21990a2681e23d0e4
Harshal CHavan <harshalchavan286@gmail.com>
2020-04-19 12:59:58 +0530

    header file commit in newcode branch

---
+++ header.h
@@ -0,0 +1,18 @@
+#include<stdio.h>
+#include<stdlib.h>
+#include<unistd.h>
+#include<string.h>
+
+#include<errno.h>
+#include<fcntl.h>
+
+#include<sys/stat.h>
+#include<sys/signal.h>
+#include<sys/types.h>
+#include<sys/wait.h>
+
+#include<netinet/in.h>
+#include<arpa/inet.h>
+
+#define MAX 1024
+#define MYPORT 19019
